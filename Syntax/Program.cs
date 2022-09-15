bdfb843c8a7860b85b8f2aef994a82f2c067500a
Ben Cofield <benrcofield@gmail.com>
2022-09-15 10:36:23 -0400

    first commit

---
+++ Syntax/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+
+int answer = 4;
+var response  = (answer < 9) ? $"{answer} is less than nine" : 
+                               $"{answer} is greater than or equal to nine";
+Console.WriteLine(response);
